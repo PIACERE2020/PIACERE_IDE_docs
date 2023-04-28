d9a0da84c82f328fb7ea5c2b46a62b64934a7217
evillanueva@prodevelop.es <PIACERE.h2020@gmail.com>
2023-04-28 09:56:37 +0200

    new update version

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ copyright = '2023, EVM'
 author = 'EVM'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -39,7 +39,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
